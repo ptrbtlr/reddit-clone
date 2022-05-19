3f2871660bb623bd675aeacfd20946940f7ac849
Cian Butler <ciandevelops@gmail.com>
2022-05-18 20:21:17 -0400

    header input and icon impts

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,24 @@
 import Image from 'next/image'
 import React from 'react'
+import {
+  ChevronDownIcon,
+  HomeIcon,
+  SearchIcon,
+  MenuIcon,
+} from '@heroicons/react/solid'
+import {
+  BellIcon,
+  ChatIcon,
+  GlobeIcon,
+  PlusIcon,
+  SparklesIcon,
+  SpeakerphoneIcon,
+  VideoCameraIcon,
+} from '@heroicons/react/outline'
 
 const Header = () => {
   return (
-    <div>
+    <div className="sticky top-0 z-50 flex bg-white px-4 py-2 shadow-sm">
       <div className="relative h-10 w-20 flex-shrink-0 cursor-pointer">
         <Image
           objectFit="contain"
@@ -11,6 +26,23 @@ const Header = () => {
           layout="fill"
         />
       </div>
+
+      <div className="mx-7 flex items-center xl:min-w-[300px]">
+        <HomeIcon className="h-5 w-5" />
+        <p className="ml-2 hidden flex-1 lg:inline">Home</p>
+        <ChevronDownIcon className="h-5 w-5" />
+      </div>
+
+      {/* SEARCH */}
+      <form className="flex flex-1 items-center space-x-2 rounded-sm border border-gray-200 bg-gray-100 px-3 py-1">
+        <SearchIcon className="h-6 w-6 text-gray-400" />
+        <input
+          type="text"
+          className="flex-1 bg-transparent outline-none"
+          placeholder="Search Reddit"
+        />
+        <button hidden type="submit" />
+      </form>
     </div>
   )
 }
